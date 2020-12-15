a555bed1e24f78d9a7948bba290023e577e0b670
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-12-15 07:42:03 -0500

    fixes test db

--- src/model/db.ts
+++ src/model/db.ts
@@ -5,14 +5,14 @@ dotenv.config();
 mongoose.Promise = global.Promise;// use es6 default promise library in mongoose
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-const mongoOptions:any = {
-  keepAlive: 1, useNewUrlParser: true, autoIndex: true, useUnifiedTopology: true,
+const mongoOptions = {
+  keepAlive: true, useNewUrlParser: true, autoIndex: true, useUnifiedTopology: true, 
 };
 /* istanbul ignore if */
 if (process.env.NODE_ENV === 'production') mongoOptions.autoIndex = false;
 let uri = process.env.MONGO_DB_URI || /* istanbul ignore next */'';
 /* istanbul ignore else */
-if (process.env.NODE_ENV === 'test') uri = process.env.TEST_MONGO || /* istanbul ignore next */'';
+if (process.env.NODE_ENV === 'test') uri = process.env.TEST_DB || /* istanbul ignore next */'';
 mongoose.connect(uri, mongoOptions);
 /* istanbul ignore next */
 mongoose.connection.on('error', (e) => {
