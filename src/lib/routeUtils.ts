035471668b7b2e36025dffea5b274a2dc6a10c23
JoshuaVSherman <joshua.v.sherman@gmail.com>
2024-02-05 08:08:10 -0500

    cleanup

--- src/lib/routeUtils.ts
+++ src/lib/routeUtils.ts
@@ -1,8 +1,7 @@
 import path from 'path';
 import type { Request, Response } from 'express';
-import { Express } from 'express-serve-static-core';
+import type { Express } from 'express-serve-static-core';
 
-// eslint-disable-next-line import/prefer-default-export
 const setRoot = (app: Express) => {
   app.get('/', (_req:Request, res:Response) => {
     res.sendFile(path.normalize(path.join(__dirname, '../../../JaMmusic/dist/index.html')));
