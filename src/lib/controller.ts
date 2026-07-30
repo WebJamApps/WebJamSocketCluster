4867a263044d5fe7dd9ecc10985bdb266b44cf9d
JoshuaVSherman <joshua.v.sherman@gmail.com>
2026-07-30 09:23:33 -0400

    feat: add centralized domain types and generic Facade/Controller abstractions

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,12 +1,14 @@
 import Debug from 'debug';
-import mongoose from 'mongoose';
+import type mongoose from 'mongoose';
+import type Facade from './facade.js';
+import type { SortOrder, QueryFilter } from '../types/index.js';
 
-const debug = Debug('WebJamSocketServer:lib-controller');
+const debug = Debug('WebJamSocketServer:controller');
 
-class Controller {
-  model: any;
+class Controller<T = Record<string, unknown>> {
+  model: Facade<T>;
 
-  constructor(model: any) {
+  constructor(model: Facade<T>) {
     this.model = model;
   }
 
@@ -16,130 +18,49 @@ class Controller {
     return Promise.resolve(true);
   }
 
-  async createDocs(body: any):Promise<any> {
+  async createDocs(body: Partial<T> | Partial<T>[]): Promise<T | T[]> {
     debug('createDocs');
-    let result: any;
+    let result: T | T[];
     try { result = await this.model.create(body); } catch (e) { return Promise.reject(e); }
     debug(result);
     return Promise.resolve(result);
   }
 
-  // async findOne(req, res) {
-  //   let book;
-  //   try {
-  //     book = await this.model.findOne(req.query);
-  //   } catch (e) { return res.status(500).json({ message: e.message }); }
-  //   if (book === undefined || book === null || book._id === null || book._id === undefined) {
-  //     return res.status(400).json({ message: 'invalid request' });
-  //   }
-  //   return res.status(200).json(book);
-  // }
-  //
-  // async findOneAndUpdate(req, res) {
-  //   let updatedBook;
-  //   try {
-  //     updatedBook = await this.model.findOneAndUpdate(req.query, req.body);
-  //   } catch (e) { return res.status(500).json({ message: e.message }); }
-  //   if (updatedBook === null || updatedBook === undefined) return res.status(400).json({ message: 'invalid request' });
-  //   return res.status(200).json(updatedBook);
-  // }
-  //
-  async getAll(): Promise<any> {
-    let collection;
+  async getAll(): Promise<T[]> {
+    let collection: T[];
     try {
       collection = await this.model.find({});
     } catch (e) { return Promise.reject(e); }
     return Promise.resolve(collection);
   }
 
-  async getAllSort(sort: any, query: any = {}): Promise<any> {
-    let collection;
+  async getAllSort(sort: SortOrder, query: QueryFilter = {}): Promise<T[]> {
+    let collection: T[];
     try {
       collection = await this.model.findSort(query, sort);
     } catch (e) { return Promise.reject(e); }
     return Promise.resolve(collection);
   }
 
-  //
-  // findById(req, res) {
-  //   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-  //     return res.status(400).send({
-  //       message: 'Find id is invalid',
-  //     });
-  //   }
-  //   return this.model.findById(req.params.id)
-  //     .then((doc) => {
-  //       if (!doc) { return res.status(400).json({ message: 'nothing found with id provided' }); }
-  //       if (doc.password !== null && doc.password !== undefined) doc.password = ''; // eslint-disable-line no-param-reassign
-  //       return res.status(200).json(doc);
-  //     });
-  // }
-  //
-  // async create(req, res) {
-  //   debug('create');
-  //   debug(req.body);
-  //   let single, created, doc;
-  //   const docArray = [];
-  //   const postBody = req.body;
-  //   if (postBody.constructor === Array) {
-  //     const arrayLength = postBody.length;
-  //     for (let i = 0; i < arrayLength; i += 1) {
-  //       try { // eslint-disable-next-line security/detect-object-injection
-  //         doc = await this.model.create(postBody[i]); // eslint-disable-line no-await-in-loop
-  //       } catch (e) {
-  //         debug(e.message);
-  //         return res.status(500).json({ message: e.message });
-  //       }
-  //       docArray.push(doc);
-  //     }
-  //     created = docArray;
-  //   } else {
-  //     try {
-  //       single = await this.model.create(req.body);
-  //     } catch (e) { return res.status(500).json({ message: e.message }); }
-  //     created = single;
-  //   }
-  //   return res.status(201).json(created);
-  // }
-  //
-  findByIdAndUpdate(id: mongoose.Types.ObjectId, body: any): Promise<any> {
+  findByIdAndUpdate(id: string | mongoose.Types.ObjectId, body: Partial<T>): Promise<T> {
+    if (!id) return Promise.reject(new Error('id is invalid'));
     return this.model.findByIdAndUpdate(id, body)
-      .then((doc: any) => {
+      .then((doc) => {
         if (!doc) return Promise.reject(new Error('Id Not Found'));
-        return Promise.resolve(doc);
+        return Promise.resolve(doc as T);
       })
       .catch((e: Error) => Promise.reject(e));
   }
 
-  deleteById(id: mongoose.Types.ObjectId): Promise<any> {
-    if (!mongoose.Types.ObjectId.isValid(id)) { return Promise.reject(new Error('id is invalid')); }
+  deleteById(id: string | mongoose.Types.ObjectId): Promise<T> {
+    if (!id) return Promise.reject(new Error('id is invalid'));
     return this.model.findByIdAndRemove(id)
-      .then((doc: any) => {
+      .then((doc) => {
         if (!doc) { return Promise.reject(new Error('Delete id not found')); }
-        return Promise.resolve(doc);
+        return Promise.resolve(doc as T);
       })
       .catch((e: Error) => Promise.reject(e));
   }
-  // findByIdAndRemove(req, res) {
-  //   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-  //     return res.status(400).send({
-  //       message: 'id is invalid',
-  //     });
-  //   }
-  //   return this.model.findByIdAndRemove(req.params.id)
-  //     .then((doc) => {
-  //       if (!doc) {
-  //         return res.status(400).send({ message: 'Delete id is invalid' });
-  //       }
-  //       return res.status(200).json({ message: `${this.model.Schema.modelName} delete was successful` });
-  //     });
-  // }
-  //
-  // deleteMany(req, res) {
-  //   return this.model.deleteMany(req.query)
-  //     .then(() => res.status(200).json({ message: `${this.model.Schema.modelName} delete was successful` }))
-  //     .catch((e) => res.status(500).json({ message: e.message }));
-  // }
 }
 
 export default Controller;
